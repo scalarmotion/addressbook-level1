d9ef51753f547bcd168b614a5c71eb1accc97820
scalarmotion <harryallenxavier@gmail.com>
2018-08-28 23:13:33 +0800

    AddressBook.java: Import HashMap class as required by commit fbe9bff045cdb300361d07b145f467aefb5d30a6

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Optional;
 import java.util.Scanner;
 import java.util.Set;
